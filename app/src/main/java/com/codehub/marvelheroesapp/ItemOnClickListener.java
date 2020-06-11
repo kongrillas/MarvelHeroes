323e912ee6af667860d0a51770e84bf1f2bde301
kongrillas <kongrillas@hotmail.com>
2020-06-11 22:51:43 +0300

    Improvements

--- app/src/main/java/com/codehub/marvelheroesapp/ItemOnClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.codehub.marvelheroesapp;
-
-import android.view.View;
-
-public interface ItemOnClickListener {
-    //DEN thn xrhsimopoiw pouthena, gia ta matia tou kosmou :p
-    void onItemClick(View view, int position);
-}
